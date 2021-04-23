10c15121342a613bc3d03bd3304c3c66ffba02fb
Ealian Kowalsky <ealiank@icloud.com>
2021-04-22 20:15:40 -0400

    Showing the user how close they are to the number.

--- game.rb
+++ game.rb
@@ -1,6 +1,20 @@
 #!/usr/bin/env ruby
 
 puts "You are playing Hot Or Cold, A simple guessing game written in ruby."
-print "I'm thinking of a number from 0 to n. Please enter n: "
+print "I'm thinking of a number from 1 to n. Please enter n: "
 n = gets.to_i
-random_number = rand(n)
+random_number = rand(1..n)
+puts "What is your guess? "
+guess = gets.to_i
+distance = (random_number - guess).abs
+if distance == 0
+  puts "Congrats!"
+elsif (1..5).include?(distance)
+  puts "You are very hot. "
+elsif (6..10).include?(distance)
+  puts "You are very hot!"
+elsif (11..15).include?(distance)
+  puts "You are warm. "
+else
+  puts "You are cold\u{1F622}"
+end

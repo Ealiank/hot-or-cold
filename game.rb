e11572cf37b8862c3a14e88f8987bae4f8ef9617
Ealian Kowalsky <ealiank@icloud.com>
2021-04-22 20:31:29 -0400

    Refactoring the conditions to use case

--- game.rb
+++ game.rb
@@ -4,17 +4,18 @@ puts "You are playing Hot Or Cold, A simple guessing game written in ruby."
 print "I'm thinking of a number from 1 to n. Please enter n: "
 n = gets.to_i
 random_number = rand(1..n)
-puts "What is your guess? "
+print "What is your guess? "
 guess = gets.to_i
 distance = (random_number - guess).abs
-if distance == 0
-  puts "Congrats!"
-elsif (1..5).include?(distance)
-  puts "You are very hot. "
-elsif (6..10).include?(distance)
+case distance
+when 0 
+  puts "Congrats! You got my number right!"
+when 1..5
   puts "You are very hot!"
-elsif (11..15).include?(distance)
+when 6..10
+  puts "You are hot!"
+when 11..15
   puts "You are warm. "
-else
+when (16..)
   puts "You are cold\u{1F622}"
 end

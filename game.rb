a88a324948f367adf25bf577756f14f194b92161
Ealian Kowalsky <ealiank@icloud.com>
2021-05-01 19:49:43 -0400

    Adding a option for how many times you can guess

--- game.rb
+++ game.rb
@@ -2,17 +2,24 @@
 
 
 puts "You are playing Hot Or Cold, A simple guessing game written in ruby. To exit the game, please press q"
+print "How many times do you want to guess? "
+num_guess = gets.to_i
 print "I'm thinking of a number from 1 to n. Please enter n: "
 n = gets.to_i
 random_number = rand(1..n)
 (1..).each do |i|
+  if i > num_guess
+    puts "You have exeeded your #{num_guess} guesses. My number was #{random_number}. "
+   break
+  end
+
   print "what is your ##{i} guess? "
   guess = gets.to_i
   if guess == 0
     print "You quit the game!"
-    break
+    break 
   end
- 
+
   distance = (random_number - guess).abs
   case distance
   when 0 

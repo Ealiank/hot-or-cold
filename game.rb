ff478b64eea1b8508066ed589e52cf34b0592858
Ealian Kowalsky <ealiank@icloud.com>
2021-05-01 19:32:07 -0400

    Adding counter to prompt and adding a quit option

--- game.rb
+++ game.rb
@@ -1,25 +1,30 @@
 #!/usr/bin/env ruby
 
 
-puts "You are playing Hot Or Cold, A simple guessing game written in ruby."
+puts "You are playing Hot Or Cold, A simple guessing game written in ruby. To exit the game, please press q"
 print "I'm thinking of a number from 1 to n. Please enter n: "
 n = gets.to_i
 random_number = rand(1..n)
-print "What is your guess? "
-loop do
+(1..).each do |i|
+  print "what is your ##{i} guess? "
   guess = gets.to_i
+  if guess == 0
+    print "You quit the game!"
+    break
+  end
+ 
   distance = (random_number - guess).abs
   case distance
   when 0 
-    puts "Congrats! You got my number right\u{1F973}"
+    puts "Congrats! You got my number right \u{1F973}"
   when 1..5
-    puts "You are very hot\u{1F92F}"
+    puts "You are very hot \u{1F92F}"
   when 6..10
-    puts "You are hot\u{1F975}"
+    puts "You are hot \u{1F975}"
   when 11..15
-    puts "You are warm\u{1F912}"
+    puts "You are warm \u{1F912}"
   when (16..)
-    puts "You are cold\u{1F976}"
+    puts "You are cold \u{1F976}"
   end
 
   break if distance == 0
